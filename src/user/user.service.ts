cd70238a7b49844f2ec251272cb8e1e5fdcee634
Godwin Onyeka <smarttin77@gmail.com>
2021-10-17 10:14:18 +0100

    user module, user entity and createUserDto

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUserDto';
+import { UserEntity } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+}
